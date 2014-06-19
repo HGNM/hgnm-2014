f0d6e655dcc5da3247dcd80e3a71f89928765c66
delucis <swithinbank@gmail.com>
2014-06-19 19:24:54 +0100

    Added WP functions for dynamic head population

--- header.php
+++ header.php
@@ -10,16 +10,17 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
-        <meta charset="utf-8">
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title><?php wp_title( '|', true, 'right' ); ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="css/normalize.min.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <?php wp_head(); ?>
     </head>
     <body>
         <!--[if lt IE 7]>

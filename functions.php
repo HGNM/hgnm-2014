d0a21608bd949d499c0de5679e9042eb8a121e3e
delucis <swithinbank@gmail.com>
2014-06-19 19:32:18 +0100

    Created functions.php with wp_title hack

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+// Ensure <title> display in case of empty loop on custom home page
+add_filter( 'wp_title', 'baw_hack_wp_title_for_home' );
+function baw_hack_wp_title_for_home( $title )
+{
+  if( empty( $title ) && ( is_home() || is_front_page() ) ) {
+    return __( 'Home', 'theme_domain' ) . ' | ' . get_bloginfo( 'title' );
+  }
+  return $title;
+}
+
+?>

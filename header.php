0a8ad75fc0555fdf422c17d5c3559bff9099a723
delucis <swithinbank@gmail.com>
2014-06-22 00:55:57 +0100

    Replaced spaces with tabs

--- header.php
+++ header.php
@@ -9,29 +9,29 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php wp_title( '|', true, 'right' ); ?></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-        <script src="<?php _e (get_stylesheet_directory_uri() . '/js/vendor/modernizr-2.6.2.min.js'); ?>"></script>
-        <?php wp_head(); ?>
-    </head>
-    <body <?php body_class(); ?>>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		
+		<script src="<?php _e (get_stylesheet_directory_uri() . '/js/vendor/modernizr-2.6.2.min.js'); ?>"></script>
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<!--[if lt IE 7]>
+			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
         
-        <header id="masthead" class="site-header js-header" role="banner">
+		<header id="masthead" class="site-header js-header" role="banner">
         	<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<nav id="menu" class="menu js-menu" role="navigation">
 				<ul>
 		        	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => 'false', 'items_wrap' => '%3$s' ) ); ?>
 				</ul>
 			</nav>
-        </header>
+		</header>

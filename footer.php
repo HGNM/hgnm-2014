07dc6139fe1ddabbeef4c746bc705db171c0020b
delucis <swithinbank@gmail.com>
2014-06-22 00:57:48 +0100

    Corrected indentation

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php _e (get_stylesheet_directory_uri() . '/js/vendor/jquery-1.11.0.min.js'); ?>"><\/script>')</script>
-	<script src="<?php _e (get_stylesheet_directory_uri() . '/js/main.js'); ?>"></script>
-	<?php wp_footer(); ?>
-</body>
 		<footer></footer>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="<?php _e (get_stylesheet_directory_uri() . '/js/vendor/jquery-1.11.0.min.js'); ?>"><\/script>')</script>
+		<script src="<?php _e (get_stylesheet_directory_uri() . '/js/main.js'); ?>"></script>
+		<?php wp_footer(); ?>
+	</body>
 </html>

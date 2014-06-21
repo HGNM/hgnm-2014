950ff5e9fe86f1645bfb7ddd308db9fe49010dbd
delucis <swithinbank@gmail.com>
2014-06-22 00:14:07 +0100

    Added dynamic file uri functions

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-	<script src="js/main.js"></script>
+	<script>window.jQuery || document.write('<script src="<?php _e (get_stylesheet_directory_uri() . '/js/vendor/jquery-1.11.0.min.js'); ?>"><\/script>')</script>
+	<script src="<?php _e (get_stylesheet_directory_uri() . '/js/main.js'); ?>"></script>
 	<?php wp_footer(); ?>
 </body>
 </html>

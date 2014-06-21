2a64d7a714dcda67fd937778533adb48a34c7225
delucis <swithinbank@gmail.com>
2014-06-22 00:05:53 +0100

    Added mobile menu script
    Pattern copied from http://a11yproject.com/

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+// Mobile menu functionality (pattern from http://a11yproject.com/)
+(function(document, window, undefined) {
 
+  'use strict';
+  
+  // Vars
+  var header = document.querySelector('.js-header'),
+    menu = document.querySelector('.js-menu'),
+    menuButton = document.createElement('button');
+
+  // Button properties
+  menuButton.classList.add('menu-button');
+  menuButton.setAttribute('id', 'menu-button');
+  menuButton.setAttribute('aria-label', 'Menu');
+  menuButton.setAttribute('aria-expanded', 'false');
+  menuButton.setAttribute('aria-controls', 'menu');
+  menuButton.innerHTML = '<span aria-hidden="true">&#x2261;</span>';
+  
+  // Menu properties
+  menu.setAttribute('aria-hidden', 'true');
+  menu.setAttribute('aria-labelledby', 'menu-button');
+  
+  // Add button to page
+  header.insertBefore(menuButton, menu);
+
+  // Handle button click event
+  menuButton.addEventListener('click', function () {
+    
+    // If active...
+    if (menu.classList.contains('active')) {
+      // Hide
+      menu.classList.remove('active');
+      menu.setAttribute('aria-hidden', 'true');
+      menuButton.setAttribute('aria-expanded', 'false');
+    } else {
+      // Show
+      menu.classList.add('active');
+      menu.setAttribute('aria-hidden', 'false');
+      menuButton.setAttribute('aria-expanded', 'true');
+
+      // Set focus on first link
+      menu.children[0].children[0].children[0].focus();
+    }
+  }, false);
+  
+})(document, window);

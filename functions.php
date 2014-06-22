68e14c8178fe5c174f749a5d87ec0fd11683f9dd
delucis <swithinbank@gmail.com>
2014-06-22 20:02:54 +0100

    Added Google Fonts include function

--- functions.php
+++ functions.php
@@ -13,4 +13,13 @@ function baw_hack_wp_title_for_home( $title )
 // Register custom menu
 register_nav_menu( 'primary', 'Primary Menu' );
 
+
+// Enqueue Google Fonts
+ function load_fonts() {
+            wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Alegreya:400italic,400,700|Alegreya+Sans:400,700');
+            wp_enqueue_style( 'googleFonts');
+        }
+    
+    add_action('wp_print_styles', 'load_fonts');
+
 ?>

d43d547f68a2599499a96b567a9b823bd05e25f3
delucis <swithinbank@gmail.com>
2014-06-26 16:34:22 +0200

    Created dynamic copyright footer function

--- functions.php
+++ functions.php
@@ -22,4 +22,27 @@ register_nav_menu( 'primary', 'Primary Menu' );
     
     add_action('wp_print_styles', 'load_fonts');
 
+// Function for dynamic copyright date in footer
+function hgnm_copyright() {
+global $wpdb;
+$copyright_dates = $wpdb->get_results("
+SELECT
+YEAR(min(post_date_gmt)) AS firstdate,
+YEAR(max(post_date_gmt)) AS lastdate
+FROM
+$wpdb->posts
+WHERE
+post_status = 'publish'
+");
+$output = '';
+if($copyright_dates) {
+$copyright = "Copyright &copy; " . $copyright_dates[0]->firstdate;
+if($copyright_dates[0]->firstdate != $copyright_dates[0]->lastdate) {
+$copyright .= '-' . $copyright_dates[0]->lastdate;
+}
+$output = $copyright;
+}
+return $output;
+}
+
 ?>

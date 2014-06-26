cecef6e4aabdc2fb35d3fdcf02d82990bad05e9b
delucis <swithinbank@gmail.com>
2014-06-26 16:14:17 +0200

    Commented out js load in footer during development

--- footer.php
+++ footer.php
@@ -3,7 +3,9 @@
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="<?php _e (get_stylesheet_directory_uri() . '/js/vendor/jquery-1.11.0.min.js'); ?>"><\/script>')</script>
+		<!-- Commented out to stop double load during development
 		<script src="<?php _e (get_stylesheet_directory_uri() . '/js/main.js'); ?>"></script>
+		-->
 		
 				<!-- Extra calls for development purposes (links to stylesheets etc. when accessing localhost remotely) -->
 				<script src="<?php _e (home_url('/wp-content/themes/hgnm-2014/js/main.js')); ?>"></script>

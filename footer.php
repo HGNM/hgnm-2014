28f2b26d4d49c63eb75f83b981f8ad5bc2c1739d
delucis <swithinbank@gmail.com>
2014-06-26 16:07:01 +0200

    Added extra stylesheet & js calls for dev access

--- footer.php
+++ footer.php
@@ -4,6 +4,11 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="<?php _e (get_stylesheet_directory_uri() . '/js/vendor/jquery-1.11.0.min.js'); ?>"><\/script>')</script>
 		<script src="<?php _e (get_stylesheet_directory_uri() . '/js/main.js'); ?>"></script>
+		
+				<!-- Extra calls for development purposes (links to stylesheets etc. when accessing localhost remotely) -->
+				<script src="<?php _e (home_url('/wp-content/themes/hgnm-2014/js/main.js')); ?>"></script>
+				<!-- End dev calls -->
+		
 		<?php wp_footer(); ?>
 	</body>
 </html>

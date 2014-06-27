068b5b3451a1114c4dc8112e50a191e073299b41
delucis <swithinbank@gmail.com>
2014-06-27 19:37:55 +0200

    Added call to link to personal website

--- single-member.php
+++ single-member.php
@@ -6,6 +6,10 @@ get_header();
 
 				<h2 class="post-title"><?php the_title(); ?></h2>
 				<div class="entry"><?php the_content(); ?></div>
+				
+				<?php if( get_field('url') ): ?>
+					<a href="<?php the_field('url'); ?>">Personal Website</a>
+				<?php endif; ?>
 
 		<?php endwhile; echo '</div>'; else: ?>
 		<?php endif;

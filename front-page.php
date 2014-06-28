bbc8232356d229aeaa97d4178845ea88000f91b9
delucis <swithinbank@gmail.com>
2014-06-28 11:50:04 +0200

    Added initial loop

--- front-page.php
+++ front-page.php
@@ -2,6 +2,16 @@
 
 get_header();
 
+		if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article id="post" <?php post_class(); ?>>
+					<h2 class="post-title"><?php the_title(); ?></h2>
+					<div class="entry"><?php the_content(); ?></div>
+				</article><!-- #post -->
+			<?php endwhile; ?>
+		<?php else: ?>
+		<?php endif;
+
 get_footer();
 
 ?>

2336d4837334144f2b25d0c3a92fef34ba465aa6
delucis <swithinbank@gmail.com>
2014-06-29 15:05:44 +0200

    Initial call for non-graduated HGNM members

--- front-page.php
+++ front-page.php
@@ -11,6 +11,25 @@ get_header();
 			<?php endwhile; ?>
 		<?php else: ?>
 		<?php endif;
+		
+		$posts = get_posts(array(
+			'numberposts' => -1,
+			'post_type' => 'member',
+			'meta_key' => 'dtend',
+			'meta_value' => ''
+		));
+		 
+		if($posts)
+		{
+			echo '<h2>Composers</h2><ul>';
+		 
+			foreach($posts as $post)
+			{
+				echo '<li><a href="' . get_permalink($post->ID) . '">' . get_the_post_thumbnail($post->ID, 'thumbnail') . '<span>' . get_the_title($post->ID) . '</span>' . '</a></li>';
+			}
+		 
+			echo '</ul>';
+		}
 
 get_footer();
 

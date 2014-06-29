1aa5fa6f9162b0f5e5f711579ff9911871bec313
delucis <swithinbank@gmail.com>
2014-06-29 15:04:23 +0200

    Corrected indentation

--- single-member.php
+++ single-member.php
@@ -8,16 +8,16 @@ get_header();
 					<h2 class="post-title"><?php the_title(); ?></h2>
 					<div class="entry"><?php the_content(); ?></div>
 					<section class="secondary">
-					<?php if( has_post_thumbnail() ): ?>
-						<div class="featured-img">
-							<?php echo get_the_post_thumbnail($post->ID, 'medium'); ?>
-						</div>
-					<?php endif; ?>
-					<?php if( get_field('url') ): ?>
-						<div class="url">
-							<a href="<?php the_field('url'); ?>">Personal Website</a>
-						</div>
-					<?php endif; ?>
+						<?php if( has_post_thumbnail() ): ?>
+							<div class="featured-img">
+								<?php echo get_the_post_thumbnail($post->ID, 'medium'); ?>
+							</div>
+						<?php endif; ?>
+						<?php if( get_field('url') ): ?>
+							<div class="url">
+								<a href="<?php the_field('url'); ?>">Personal Website</a>
+							</div>
+						<?php endif; ?>
 					</section>
 				</article><!-- #post -->
 			<?php endwhile; ?>

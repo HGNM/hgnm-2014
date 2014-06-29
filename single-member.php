1701c3b808b6c4356c7420d103f55fc5118b9c39
delucis <swithinbank@gmail.com>
2014-06-29 13:10:13 +0200

    Added post thumbnail call

--- single-member.php
+++ single-member.php
@@ -7,6 +7,11 @@ get_header();
 				<article id="post" <?php post_class(); ?>>
 					<h2 class="post-title"><?php the_title(); ?></h2>
 					<div class="entry"><?php the_content(); ?></div>
+					<?php if( has_post_thumbnail() ): ?>
+						<div class="featured-img">
+							<?php echo get_the_post_thumbnail($post->ID, 'medium'); ?>
+						</div>
+					<?php endif; ?>
 					<?php if( get_field('url') ): ?>
 						<a href="<?php the_field('url'); ?>">Personal Website</a>
 					<?php endif; ?>

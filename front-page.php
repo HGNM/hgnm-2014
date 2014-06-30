5b13b4b40852907253fb8f8cf8fe7e84068ffb32
delucis <swithinbank@gmail.com>
2014-07-01 01:14:11 +0200

    Created functions fetching upcoming events

--- front-page.php
+++ front-page.php
@@ -12,6 +12,40 @@ get_header();
 		<?php else: ?>
 		<?php endif;
 		
+		// Get upcoming concerts
+		$today = date('Ymd', strtotime('-1 day'));
+		$concerts = get_posts(
+			array(
+				'numberposts' => 1,
+				'post_type' => 'concert',
+				'meta_key' => 'dtstart',
+				'order' => 'ASC',
+				'meta_query' => array(
+					array(
+						'key' => 'dtstart',
+                        'value'  => $today,
+                        'compare'  => '>'
+					)
+				)
+			)
+		);
+		// Get upcoming colloquia
+		$colloquia = get_posts(
+			array(
+				'numberposts' => 3,
+				'post_type' => 'colloquium',
+				'meta_key' => 'dtstart',
+				'order' => 'ASC',
+				'meta_query' => array(
+					array(
+						'key' => 'dtstart',
+                        'value'  => $today,
+                        'compare'  => '>'
+					)
+				)
+			)
+		);
+		
 		// Get composers names, photos and permalinks
 		$posts = get_posts(array(
 			'numberposts' => -1,

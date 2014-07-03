a69fcab6e24eaec4769deea9ff44c0b6c2f2896a
delucis <swithinbank@gmail.com>
2014-07-03 16:10:54 +0200

    Added very basic loop as fallback

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
 get_header();
 
+		if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class('p-section primary entry'); ?>>
+					<h2 class="post-title fname"><?php the_title(); ?></h2>
+						<?php the_content(); ?>
+				</article>
+			<?php endwhile; ?>
+		<?php else: ?>
+		<?php endif;
+
 get_footer();
 
 ?>

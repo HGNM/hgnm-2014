af1b380a046aa6c61b77380a3f757de67107732c
delucis <swithinbank@gmail.com>
2014-07-04 17:46:24 +0200

    Fetched and validated query variable

--- archive.php
+++ archive.php
@@ -2,6 +2,16 @@
 
 get_header();
 
+		// If there is a year query, e.g. '?y=2012', get it and use it as a variable, else set it to the current year
+		if(get_query_var('y')) {
+			if (filter_var(get_query_var('y'), FILTER_VALIDATE_INT, array('options' => array ('min_range' => 0, 'max_range' => 9999,))) !== FALSE) {
+				$yearquery = get_query_var('y');
+			}
+		}
+		else {
+			$yearquery = date('Y');
+		}
+		
 		if ( have_posts() ) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class('p-section primary entry'); ?>>

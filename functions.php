a35f072cac7aca897547207f90243f60b04e937d
delucis <swithinbank@gmail.com>
2014-07-05 01:42:57 +0200

    Adapted home page hack for new title method

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ add_filter( 'wp_title', 'baw_hack_wp_title_for_home' );
 function baw_hack_wp_title_for_home( $title )
 {
   if( empty( $title ) && ( is_home() || is_front_page() ) ) {
-    return __( 'Home', 'theme_domain' ) . ' | ' . get_bloginfo( 'title' );
+    return __( 'Home', 'theme_domain' );
   }
   return $title;
 }

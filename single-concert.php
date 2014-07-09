bbf81d07c5be3cb51f97de882419e7037ad44485
delucis <swithinbank@gmail.com>
2014-07-09 13:19:17 +0200

    Rewrote programme parsing functions

--- single-concert.php
+++ single-concert.php
@@ -17,15 +17,54 @@ get_header();
 				<?php if(have_rows('programme') || have_rows('programme_plus')): ?>
 					<div class="programme">
 						<h3>Programme</h3>
-						<ul>
 							<?php
+							// Get programme data from custom fields
 							$programme = get_field('programme');
 							$programmeplus = get_field('programme_plus');
-							$count =  count($programme);
+
+							// Create empty array to populate with programme data
+							$fullprogramme = array();
+							
+							// For each programme item create an array and append it to the $fullprogramme array
+							foreach ($programme as $item) {
+								$id = $item['composer'];
+								$composer = get_the_title($id->ID);
+								$url = get_permalink($id->ID);
+								$work_title = $item['work_title'];
+								$embed_link = $item['embed_link'];
+								$a_or_v = $item['a_or_v'];
+								$row = array(array('composer' => $composer, 'url' => $url, 'work_title' => $work_title, 'embed_link' => $embed_link, 'a_or_v' => $a_or_v));
+								$fullprogramme = array_merge($fullprogramme, $row);
+							}
+							
+							// For each programme plus item create an array and append it to the $fullprogramme array
+							foreach ($programmeplus as $item) {
+								$composer = $item['composer'];
+								$url = '';
+								$work_title = $item['work_title'];
+								$embed_link = $item['embed_link'];
+								$a_or_v = $item['a_or_v'];
+								$row = array(array('composer' => $composer, 'url' => $url, 'work_title' => $work_title, 'embed_link' => $embed_link, 'a_or_v' => $a_or_v));
+								$fullprogramme = array_merge($fullprogramme, $row);
+							}
+							
+							// Set up list of names to sort by
+							foreach ( $fullprogramme as $key => $row ) {
+								$column_id[$key]=$row['composer'];
+							}
+							
+							// Sort programme by composer name
+							array_multisort($column_id, SORT_STRING, $fullprogramme);
+							
+							
+							// Count programme items
+							$count =  count($fullprogramme);
+							
+							// Test to see if there’s a complete list of work titles
 							$i = 0;
 							$hasworks = 1; 
 							while ($i < $count) {
-								if (strlen($programme[$i]['work_title']) != 0) {
+								if (strlen($fullprogramme[$i]['work_title']) != 0) {
 								}
 								else {
 									$hasworks = 0;
@@ -33,72 +72,72 @@ get_header();
 								}
 								$i++;
 							}
+							
+							// Output if there is a complete list of work titles
+							// Format: 	<li>
+							//				<strong class="composer">COMPOSER-NAME</strong><br />
+							// 				<em class="work_title">WORK-TITLE</em>
+							//			</li>
+							// With a link wrapped around COMPOSER-NAME where present
 							if ($hasworks) {
-								// Output if there is a complete list of work titles
-								while (have_rows('programme')) {
-									the_row();
-									if (get_sub_field('composer')) {
-										$composerid = get_sub_field('composer'); ?>
-											<li>
-												<strong class="composer"><a href="<?php echo esc_url( get_permalink($composerid->ID) ); ?>"><?php echo get_the_title($composerid->ID); ?></a><br /></strong>
-												<em class="work_title"><?php the_sub_field('work_title'); ?></em>
-											</li>
-										<?php }
-									}
-								}
-							else {
-								// Output if incomplete list of work titles
-								$n = 0;
-								echo 'Works by ';
-								while ($n < $count) {
-									if ($n < ($count - 2)) {
-										$composerid = $programme[$n]['composer'];
-										echo '<a href="' . esc_url( get_permalink($composerid->ID) ) . '">' . get_the_title($composerid->ID) . '</a>, ';
-										$n++;
-									}
-									elseif ($n < ($count - 1)) {
-										$composerid = $programme[$n]['composer'];
-										echo '<a href="' . esc_url( get_permalink($composerid->ID) ) . '">' . get_the_title($composerid->ID) . '</a> and ';
-										$n++;
+								echo '<ul>';
+								foreach ($fullprogramme as $item) {
+									echo '<li><strong class="composer">';
+									if ($item['url']) {
+										echo '<a href="' . esc_url( $item['url'] ) . '">';
+										echo $item['composer'] . '</a>';
 									}
 									else {
-										$composerid = $programme[$n]['composer'];
-										echo '<a href="' . esc_url( get_permalink($composerid->ID) ) . '">' . get_the_title($composerid->ID) . '.';
-										$n++;
+										echo $item['composer'];
 									}
+									echo '</strong><br /><em class="work_title">' . $item['work_title'] . '</em>';
+									echo '</li>';
 								}
+								echo '</ul>';
 							}
 
+							else {
+								// Output if incomplete list of work titles
+								// Format: a list of composer names seperated by commas, final pair joined by ' and '
+								$n = 0;
+								echo '<p>Works by ';
+								foreach ($fullprogramme as $item) {
+									if ($n == ($count - 1)) {
+										// last item
+										if ($item['url']) {
+											echo '<a href="' . esc_url( $item['url'] ) . '">';
+											echo $item['composer'] . '</a>.';
+										}
+										else {
+											echo $item['composer'] . '.';
+										}
+									}
+									elseif ($n == ($count - 2)) {
+										// penultimate item
+										if ($item['url']) {
+											echo '<a href="' . esc_url( $item['url'] ) . '">';
+											echo $item['composer'] . '</a> and ';
+										}
+										else {
+											echo $item['composer'] . ' and ';
+										}
+									}
+									else {
+										// other items
+										if ($item['url']) {
+											echo '<a href="' . esc_url( $item['url'] ) . '">';
+											echo $item['composer'] . '</a>, ';
+										}
+										else {
+											echo $item['composer'] . ', ';
+										}
+									}
+									$n++;
+								}
+								echo '</p>';
 
-							// OLD OUTPUT FOR REFERENCE WHILE BUILDING NEW ?><!-- <?php
-							
-							while( have_rows('programme') ): the_row(); ?>
-								<?php if (get_sub_field('composer')) : ?>
-									<?php $composerid = get_sub_field('composer'); ?>
-									<?php if (get_sub_field('work_title')) : ?>
-										<li>
-											<strong class="composer"><a href="<?php echo esc_url( get_permalink($composerid->ID) ); ?>"><?php echo get_the_title($composerid->ID); ?></a><br /></strong>
-											<em class="work_title"><?php the_sub_field('work_title'); ?></em>
-										</li>
-									<?php endif; ?>
-									<?php if (!get_sub_field('work_title')) : ?>
-										<a href="<?php echo esc_url( get_permalink($composerid->ID) ); ?>"><?php echo get_the_title($composerid->ID); ?></a>, 
-									<?php endif; ?>
-								<?php endif; ?>
-							<?php endwhile; ?>
-							<?php while( have_rows('programme_plus') ): the_row(); ?>
-								<?php if (get_sub_field('composer') && get_sub_field('work_title')) : ?>
-									<li>
-										<strong class="composer"><?php the_sub_field('composer'); ?><br /></strong>
-										<em class="work_title"><?php the_sub_field('work_title'); ?></em>
-									</li>
-								<?php endif; ?>
-							<?php endwhile; ?>
-							
-							-->
-							
-							
-						</ul>
+							} ?>
+
 					</div><!-- .programme -->
 				<?php endif; ?>
 			</article><!-- #posts -->

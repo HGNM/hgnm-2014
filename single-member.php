6cb78296eaa18f7ac7c6ff4035e787ce63773fc2
delucis <swithinbank@gmail.com>
2014-07-14 20:47:13 +0200

    Moved array set-up to top of post

--- single-member.php
+++ single-member.php
@@ -3,7 +3,98 @@
 get_header();
 
 		if ( have_posts() ) : ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post();
+			
+				// Get composer ID to look for
+				$testID = get_the_ID();
+				
+				// Get archived colloquia
+				$colloquia = get_posts(
+					array(
+						'numberposts' => -1,
+						'post_type' => 'colloquium',
+						'meta_key' => 'dtstart',
+						'orderby' => 'dtstart',
+						'order' => 'ASC',
+						'meta_query' => array(
+							array(
+								'key' => 'fname',
+				                      'value'  => $testID,
+							)
+						)
+					)
+				);
+				
+				// custom filter to replace '=' with 'LIKE'
+				function my_posts_where( $where ) {
+					$where = str_replace("meta_key = 'programme_%_composer'", "meta_key LIKE 'programme_%_composer'", $where);
+					return $where;
+				}
+				 
+				add_filter('posts_where', 'my_posts_where');
+				
+				// Get archived concerts
+				$concerts = get_posts(
+					array(
+						'suppress_filters' => FALSE,
+						'numberposts' => -1,
+						'post_type' => 'concert',
+						'meta_key' => 'dtstart',
+						'orderby' => 'dtstart',
+						'order' => 'ASC',
+						'meta_query' => array(
+							array(
+								'key' => 'programme_%_composer',
+				                      'value'  => $testID,
+							)
+						)
+					)
+				);
+				
+				$upcomingcolloquia = $colloquia;
+				$pastcolloquia = $colloquia;
+				$upcomingconcerts = $concerts;
+				$pastconcerts = $concerts;
+				date_default_timezone_set('America/New_York');
+				
+				// Unset array items in the past — COLLOQUIA
+				foreach ($upcomingcolloquia as $key => $row) {
+					$dtstart = get_field('dtstart', $row->ID) . ' 12:00';
+					$dtstart = DateTime::createFromFormat('d/m/Y G:i', $dtstart);
+					if (($dtstart->format('Ymd')) < date('Ymd')) {
+						unset($upcomingcolloquia[$key]);
+					}
+				}
+				
+				// Unset array items in the future (or today) — COLLOQUIA
+				foreach ($pastcolloquia as $key => $row) {
+					$dtstart = get_field('dtstart', $row->ID) . ' 12:00';
+					$dtstart = DateTime::createFromFormat('d/m/Y G:i', $dtstart);
+					if (($dtstart->format('Ymd')) >= date('Ymd')) {
+						unset($pastcolloquia[$key]);
+					}
+				}
+				
+				// Unset array items in the past — CONCERTS
+				foreach ($upcomingconcerts as $key => $row) {
+					$dtstart = get_field('dtstart', $row->ID) . ' 12:00';
+					$dtstart = DateTime::createFromFormat('d/m/Y G:i', $dtstart);
+					if (($dtstart->format('Ymd')) < date('Ymd')) {
+						unset($upcomingconcerts[$key]);
+					}
+				}
+				
+				// Unset array items in the future (or today) — CONCERTS
+				foreach ($pastconcerts as $key => $row) {
+					$dtstart = get_field('dtstart', $row->ID) . ' 12:00';
+					$dtstart = DateTime::createFromFormat('d/m/Y G:i', $dtstart);
+					if (($dtstart->format('Ymd')) >= date('Ymd')) {
+						unset($pastconcerts[$key]);
+					}
+				}
+				
+				?>
+			
 				<article id="post-<?php the_ID(); ?>" <?php post_class('p-section'); ?>>
 					<h2 class="post-title fname"><?php the_title(); ?></h2>
 					<section class="primary entry">
@@ -20,94 +111,8 @@ get_header();
 								<a href="<?php the_field('url'); ?>">Personal Website</a>
 							</div>
 						<?php endif;
-						
-						// Get composer ID to look for
-						$testID = get_the_ID();
-						
-						// Get archived colloquia
-						$colloquia = get_posts(
-							array(
-								'numberposts' => -1,
-								'post_type' => 'colloquium',
-								'meta_key' => 'dtstart',
-								'orderby' => 'dtstart',
-								'order' => 'ASC',
-								'meta_query' => array(
-									array(
-										'key' => 'fname',
-				                        'value'  => $testID,
-									)
-								)
-							)
-						);
-						
-						// custom filter to replace '=' with 'LIKE'
-						function my_posts_where( $where ) {
-							$where = str_replace("meta_key = 'programme_%_composer'", "meta_key LIKE 'programme_%_composer'", $where);
-							return $where;
-						}
-						 
-						add_filter('posts_where', 'my_posts_where');
-						
-						// Get archived concerts
-						$concerts = get_posts(
-							array(
-								'suppress_filters' => FALSE,
-								'numberposts' => -1,
-								'post_type' => 'concert',
-								'meta_key' => 'dtstart',
-								'orderby' => 'dtstart',
-								'order' => 'ASC',
-								'meta_query' => array(
-									array(
-										'key' => 'programme_%_composer',
-				                        'value'  => $testID,
-									)
-								)
-							)
-						);
-						
-						$upcomingcolloquia = $colloquia;
-						$pastcolloquia = $colloquia;
-						$upcomingconcerts = $concerts;
-						$pastconcerts = $concerts;
-						date_default_timezone_set('America/New_York');
-						
-						// Unset array items in the past — COLLOQUIA
-						foreach ($upcomingcolloquia as $key => $row) {
-							$dtstart = get_field('dtstart', $row->ID) . ' 12:00';
-							$dtstart = DateTime::createFromFormat('d/m/Y G:i', $dtstart);
-							if (($dtstart->format('Ymd')) < date('Ymd')) {
-								unset($upcomingcolloquia[$key]);
 							}
-						}
-						
-						// Unset array items in the future (or today) — COLLOQUIA
-						foreach ($pastcolloquia as $key => $row) {
-							$dtstart = get_field('dtstart', $row->ID) . ' 12:00';
-							$dtstart = DateTime::createFromFormat('d/m/Y G:i', $dtstart);
-							if (($dtstart->format('Ymd')) >= date('Ymd')) {
-								unset($pastcolloquia[$key]);
 							}
-						}
-						
-						// Unset array items in the past — CONCERTS
-						foreach ($upcomingconcerts as $key => $row) {
-							$dtstart = get_field('dtstart', $row->ID) . ' 12:00';
-							$dtstart = DateTime::createFromFormat('d/m/Y G:i', $dtstart);
-							if (($dtstart->format('Ymd')) < date('Ymd')) {
-								unset($upcomingconcerts[$key]);
-							}
-						}
-						
-						// Unset array items in the future (or today) — CONCERTS
-						foreach ($pastconcerts as $key => $row) {
-							$dtstart = get_field('dtstart', $row->ID) . ' 12:00';
-							$dtstart = DateTime::createFromFormat('d/m/Y G:i', $dtstart);
-							if (($dtstart->format('Ymd')) >= date('Ymd')) {
-								unset($pastconcerts[$key]);
-							}
-						}
 						
 						// Display Next Colloquium
 						if ($upcomingcolloquia) {

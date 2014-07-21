3d928e62c5e8969dfc15caaa4ad376dd1ae3753f
delucis <swithinbank@gmail.com>
2014-07-21 02:29:28 +0200

    Added composer-list link

--- single-member.php
+++ single-member.php
@@ -170,6 +170,9 @@ get_header();
 							
 						</section> <!-- .secondary -->
 					<?php endif; ?>
+					<section class="composers-link">
+						<?php echo '<a href="' . get_post_type_archive_link('member') . '">See all composers</a>'; ?>
+					</section>
 				</article><!-- #post -->
 			<?php endwhile; ?>
 		<?php else: ?>

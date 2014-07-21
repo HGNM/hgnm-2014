eaaf3aeb57e66de09464a9be2f94e4ac6728ed06
delucis <swithinbank@gmail.com>
2014-07-22 01:24:09 +0200

    Added language attribute to site body

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 		
 		<?php wp_head(); ?>
 	</head>
-	<body <?php body_class(); ?>>
+	<body <?php body_class(); ?> lang="en">
 		<!--[if lt IE 7]>
 			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 		<![endif]-->

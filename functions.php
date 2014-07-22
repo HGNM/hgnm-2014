48ce8ef197651f671298437ffa6117df13b5dfc2
delucis <swithinbank@gmail.com>
2014-07-22 23:40:37 +0200

    Registered colloquium custom post type

--- functions.php
+++ functions.php
@@ -150,6 +150,55 @@ function concert_post_type() {
 // Hook into the 'init' action
 add_action( 'init', 'concert_post_type', 0 );
 
+// Register Colloquium Custom Post Type
+function colloquium_post_type() {
+
+	$labels = array(
+		'name'                => _x( 'Colloquia', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Colloquium', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Colloquia', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+		'all_items'           => __( 'All Colloquia', 'text_domain' ),
+		'view_item'           => __( 'View Colloquium', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Colloquium', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Colloquium', 'text_domain' ),
+		'update_item'         => __( 'Update Colloquium', 'text_domain' ),
+		'search_items'        => __( 'Search Colloquia', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                => 'colloquium',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => true,
+	);
+	$args = array(
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-format-chat',
+		'can_export'          => true,
+		'has_archive'         => 'events',
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'colloquium', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'colloquium_post_type', 0 );
+
 // Enable Featured Image for Member Custom Post Type
 if ( function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails', array( 'member', 'concert', 'miscevent' ) );

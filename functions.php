0f456d1f6bc42f4a0ff407c452447210839a41b0
delucis <swithinbank@gmail.com>
2014-07-22 23:39:59 +0200

    Registered concert custom post type

--- functions.php
+++ functions.php
@@ -99,6 +99,57 @@ function member_post_type() {
 // Hook into the 'init' action
 add_action( 'init', 'member_post_type', 0 );
 
+// Register Concert Custom Post Type
+function concert_post_type() {
+
+	$labels = array(
+		'name'                => _x( 'Concerts', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Concert', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Concerts', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+		'all_items'           => __( 'All Concerts', 'text_domain' ),
+		'view_item'           => __( 'View Concert', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Concert', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Concert', 'text_domain' ),
+		'update_item'         => __( 'Update Concert', 'text_domain' ),
+		'search_items'        => __( 'Search Concerts', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                => 'concert',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => true,
+	);
+	$args = array(
+		'label'               => __( 'concert', 'text_domain' ),
+		'description'         => __( 'Entry containing information about an HGNM concert', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-format-audio',
+		'can_export'          => true,
+		'has_archive'         => 'archives',
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'concert', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'concert_post_type', 0 );
+
 // Enable Featured Image for Member Custom Post Type
 if ( function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails', array( 'member', 'concert', 'miscevent' ) );

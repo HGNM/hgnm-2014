1e55c722024bf222873a6eecc6d0398b43926b19
delucis <swithinbank@gmail.com>
2014-07-22 23:39:27 +0200

    Registered member custom post type

--- functions.php
+++ functions.php
@@ -48,6 +48,57 @@ $output = $copyright;
 return $output;
 }
 
+// Register Member Custom Post Type
+function member_post_type() {
+
+	$labels = array(
+		'name'                => _x( 'Members', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Member', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Members', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+		'all_items'           => __( 'All Members', 'text_domain' ),
+		'view_item'           => __( 'View Member', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Member', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Member', 'text_domain' ),
+		'update_item'         => __( 'Update Member', 'text_domain' ),
+		'search_items'        => __( 'Search Members', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                => 'composer',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => true,
+	);
+	$args = array(
+		'label'               => __( 'member', 'text_domain' ),
+		'description'         => __( 'Entry containing information about an HGNM member', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'thumbnail', ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-id-alt',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'member', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'member_post_type', 0 );
+
 // Enable Featured Image for Member Custom Post Type
 if ( function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails', array( 'member', 'concert', 'miscevent' ) );

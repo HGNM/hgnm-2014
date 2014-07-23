1fc394c91e8df187ed5437154f68374c45ded497
delucis <swithinbank@gmail.com>
2014-07-24 01:30:49 +0200

    Created functions to display flexible content

--- single-miscevent.php
+++ single-miscevent.php
@@ -104,236 +104,74 @@ get_header();
 				</section> <!-- event meta -->
 
 				<?php
-				// SUMMARY FIELD
-				if( get_field('summary') ): ?>
-				<section class="description">
-					<?php the_field('summary'); ?>
-				</section>
-				<?php endif; ?>
-
-				<?php
-				// PROGRAMME CONTENT
-				if(have_rows('programme') || have_rows('programme_plus')): ?>
-					<section class="programme">
-						<h3>Program</h3>
-							<?php
-							// Get programme data from custom fields
-							$programme = get_field('programme');
-							$programmeplus = get_field('programme_plus');
-
-							// Create empty array to populate with programme data
-							$fullprogramme = array();
-							
-							// For each programme item create an array and append it to the $fullprogramme array
-							foreach ($programme as $item) {
-								$id = $item['composer'];
-								$composer = get_the_title($id->ID);
-								$url = get_permalink($id->ID);
-								$work_title = $item['work_title'];
-								$embed_link = $item['embed_link'];
-								$a_or_v = $item['a_or_v'];
-								$row = array(array('composer' => $composer, 'url' => $url, 'work_title' => $work_title, 'embed_link' => $embed_link, 'a_or_v' => $a_or_v));
-								$fullprogramme = array_merge($fullprogramme, $row);
-							}
-							
-							// For each programme plus item create an array and append it to the $fullprogramme array
-							foreach ($programmeplus as $item) {
-								$composer = $item['composer'];
-								$url = '';
-								$work_title = $item['work_title'];
-								$embed_link = $item['embed_link'];
-								$a_or_v = $item['a_or_v'];
-								$row = array(array('composer' => $composer, 'url' => $url, 'work_title' => $work_title, 'embed_link' => $embed_link, 'a_or_v' => $a_or_v));
-								$fullprogramme = array_merge($fullprogramme, $row);
-							}
-							
-							// Set up list of names to sort by
-							foreach ( $fullprogramme as $key => $row ) {
-								$column_id[$key]=$row['composer'];
-							}
-							
-							// Sort programme by composer name
-							array_multisort($column_id, SORT_STRING, $fullprogramme);
-							
-							
-							// Count programme items
-							$count =  count($fullprogramme);
-							
-							// Test to see if there’s a complete list of work titles
-							$i = 0;
-							$hasworks = 1; 
-							while ($i < $count) {
-								if (strlen($fullprogramme[$i]['work_title']) != 0) {
+				if( have_rows('details') ) {
+					while ( have_rows('details') ) {
+						the_row();
+						// SUMMARY FIELD
+						if( get_row_layout() == 'description' ) : ?>
+							<section class="description">
+								<h3>About this event</h3>
+								<?php the_sub_field('summary'); ?>
+							</section>
+						<?php endif;
+						
+						// EXTERNAL LINK
+						if ( get_row_layout() == 'link' ) {
+							echo '<section class="external-links"><ul><li><a href="' . get_sub_field('url') . '">' . get_sub_field('title') . ' »</a></li></ul></section>';
+						}
+						
+						// ARCHIVE CONTENT
+						if ( get_row_layout() == 'documents' ) {
+							if( have_rows('document') ) {
+								echo '<section class="archive-docs"><h3>Related Files</h3><ul>';
+								while ( have_rows('document') ) {
+									the_row();
+									if ( get_sub_field('file') && get_sub_field('file_name') ) {
+										$document = get_sub_field('file');
+										echo '<li><span class="icon icon-download" aria-hidden="true"></span><a href="' . esc_url($document['url']) . '">' . get_sub_field('file_name') . '</a></li>';
+									}
 								}
-								else {
-									$hasworks = 0;
-									break;
+								echo '</ul></section>';
+							}
+						}
+						
+						// VIDEOS
+						if ( get_row_layout() == 'videos' ) {
+							if( have_rows('videos') ) {
+								echo '<section class="multimedia"><h3>Videos</h3><ul class="video clearfix">';
+								while ( have_rows('videos') ) {
+									the_row();
+									if ( get_sub_field('embed_link') ) {
+										$embed = get_sub_field('embed_link');
+										echo '<li><span class="embed-container">' . $embed . '</span></li>';
+									}
 								}
-								$i++;
+								echo '</ul></section>';
 							}
-							
-							// Output if there is a complete list of work titles
-							// Format: 	<li>
-							//				<strong class="composer">COMPOSER-NAME</strong><br />
-							// 				<em class="work_title">WORK-TITLE</em>
-							//			</li>
-							// With a link wrapped around COMPOSER-NAME where present
-							if ($hasworks) {
-								echo '<ul>';
-								foreach ($fullprogramme as $item) {
-									echo '<li><strong class="composer">';
-									if ($item['url']) {
-										echo '<a href="' . esc_url( $item['url'] ) . '">';
-										echo $item['composer'] . '</a>';
-									}
-									else {
-										echo $item['composer'];
-									}
-									echo '</strong> <em class="work_title">' . $item['work_title'] . '</em>';
-									echo '</li>';
-								}
-								echo '</ul>';
-							}
-
-							else {
-								// Output if incomplete list of work titles
-								// Format: a list of composer names seperated by commas, final pair joined by ' and '
-								$n = 0;
-								echo '<p class="no-works">Works by ';
-								foreach ($fullprogramme as $item) {
-									if ($n == ($count - 1)) {
-										// last item
-										if ($item['url']) {
-											echo '<a href="' . esc_url( $item['url'] ) . '">';
-											echo $item['composer'] . '</a>.';
-										}
-										else {
-											echo $item['composer'] . '.';
-										}
-									}
-									elseif ($n == ($count - 2)) {
-										// penultimate item
-										if ($item['url']) {
-											echo '<a href="' . esc_url( $item['url'] ) . '">';
-											echo $item['composer'] . '</a> and ';
-										}
-										else {
-											echo $item['composer'] . ' and ';
-										}
-									}
-									else {
-										// other items
-										if ($item['url']) {
-											echo '<a href="' . esc_url( $item['url'] ) . '">';
-											echo $item['composer'] . '</a>, ';
-										}
-										else {
-											echo $item['composer'] . ', ';
-										}
-									}
-									$n++;
-								}
-								echo '</p>';
-
-							} ?>
-
-					</section><!-- .programme -->
-				<?php endif; ?>
-				
-				<?php
-				// ARCHIVE CONTENT
-				if( get_field('poster_pdf') || get_field('programme_pdf') ) {
-					echo '<section class="archive-docs"><h3>Related Files</h3><ul>';
-					if( get_field('poster_pdf') ) {
-						$posterpdf = get_field('poster_pdf');
-						if ($posterpdf['mime_type'] == 'application/pdf') {
-							echo '<li><span class="icon icon-download" aria-hidden="true"></span><a href="' . esc_url($posterpdf['url']) . '">Download PDF of concert poster</a></li>';
 						}
-						else {
-							echo '<li><span class="icon icon-download" aria-hidden="true"></span><a href="' . esc_url($posterpdf['url']) . '">Download concert poster</a></li>';
+						
+						// PHOTOS
+						if ( get_row_layout() == 'photos' ) {
+							$images = get_sub_field('gallery');
+							if( $images ): ?>
+							<section class="gallery">
+								<h3>Photos</h3>
+							    <ul class="popup-gallery clearfix">
+							        <?php foreach( $images as $image ): ?>
+							            <li>
+							            	<a href="<?php echo $image['sizes']['large']; ?>" title="<?php echo $image['title']; ?>">
+							            		 <img src="<?php echo $image['sizes']['hgnm-thumb']; ?>" alt="<?php echo $image['alt']; ?>" />
+							            	</a>
+							            </li>
+							        <?php endforeach; ?>
+							    </ul>
+						    </section>
+							<?php endif;
 						}
-					}
-					if( get_field('programme_pdf') ) {
-						$programme = get_field('programme_pdf');
-						if ($programme['mime_type'] == 'application/pdf') {
-							echo '<li><span class="icon icon-download" aria-hidden="true"></span><a href="' . esc_url($programmepdf['url']) . '">Download PDF of program booklet</a></li>';
-						}
-						else {
-							echo '<li><span class="icon icon-download" aria-hidden="true"></span><a href="' . esc_url($programmepdf['url']) . '">Download program booklet</a></li>';
-						}
-					}
-					echo '</ul></section>';
-				}
-				?>
-				
-				<?php if(get_field('performer_url') || get_field('facebook_url')) : ?>
-					<section class="external-links clearfix <?php if(get_field('performer_url') & get_field('facebook_url')) { echo 'both'; } ?>">
-						<ul>
-							<?php
-							// PERFORMER LINK
-							if( get_field('performer_url') ): ?>
-								<li><a href="<?php esc_url( the_field('performer_url') ); ?>">Find out more about <span class="perfname"><?php the_title(); ?><span class="icon icon-link-ext" aria-hidden="true"></span></span></a></li>
-							<?php endif; ?>
-							<?php
-							// FACEBOOK LINK
-							if( get_field('facebook_url') ): ?>
-								<li><a href="<?php esc_url( the_field('facebook_url') ); ?>">See this event on Facebook <span class="icon icon-facebook" aria-hidden="true"></span></a></li>
-							<?php endif; ?>
-						</ul>
-					</section>
-				<?php endif; ?>
-				
-				<?php
-				// A/V CONTENT
-				if( get_field('a_v') ) {
-					$audiolist = $fullprogramme;
-					$videolist = $fullprogramme;
-					foreach($audiolist as $key => $row) {
-						if($row['a_or_v'] !== 'Audio') unset($audiolist[$key]);
-					}
-					foreach($videolist as $key => $row) {
-						if($row['a_or_v'] !== 'Video') unset($videolist[$key]);
-					}
-					if($audiolist || $videolist) {
-						echo '<section class="multimedia"><ul>';
-						if ($audiolist) {
-							echo '<li class="audio clearfix"><h3>Audio</h3><ul>';
-							foreach($audiolist as $item) {
-								echo '<li><span class="embed-container">' . $item['embed_link'] . '</span></li>';
-							}
-							echo '</ul></li>';
-						}
-						if ($videolist) {
-							echo '<li class="video clearfix"><h3>Video</h3><ul>';
-							foreach($videolist as $item) {
-									echo '<li><span class="embed-container">' . $item['embed_link'] . '</span></li>';
-							}
-							echo '</ul></li>';
-						}
-						echo '</ul></section>';
 					}
 				}
 				?>
-				
-				<?php 
-				// GALLERY
-				$images = get_field('gallery');
-				 
-				if( $images ): ?>
-				<section class="gallery">
-					<h3>Photos</h3>
-				    <ul class="popup-gallery clearfix">
-				        <?php foreach( $images as $image ): ?>
-				            <li>
-				            	<a href="<?php echo $image['url']; ?>" title="<?php echo $image['title']; ?>">
-				            		 <img src="<?php echo $image['sizes']['hgnm-thumb']; ?>" alt="<?php echo $image['alt']; ?>" />
-				            	</a>
-				            </li>
-				        <?php endforeach; ?>
-				    </ul>
-			    </section>
-				<?php endif; ?>
-				
+								
 				<section class="archive-link">
 				<?php
 				//ARCHIVE LINK - shows link to archive for season that includes this concert

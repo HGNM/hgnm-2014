e169c0018470060929a3c11446469664fd28186e
delucis <swithinbank@gmail.com>
2014-07-24 01:46:12 +0200

    Added date & location display

--- single-colloquium.php
+++ single-colloquium.php
@@ -2,24 +2,33 @@
 
 get_header();
 
-		if ( have_posts() ) {
-			echo '<div id="posts">';
+		if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class('p-section primary entry'); ?>>
+					<h2 class="post-title fname"><?php the_title(); ?></h2>
+					<?php if (current_user_can('edit_post')) : ?>
+						<a href="<?php echo get_edit_post_link(); ?>" class="edit-button">Edit</a>
+					<?php endif; ?>
+						<?php
+						// SET TIMEZONE
+						date_default_timezone_set('America/New_York');
+						$dtstart = DateTime::createFromFormat('d/m/Y G:i', (get_field('dtstart') . ' 12:00'));
+						
+						// EVENT META — date, time & location
+						?>
+						<section class="event-meta">
+							<p class="dtstart"><time class="value" datetime="<?php echo $dtstart->format('Y-m-d\TH:i:sO'); ?>">
+								<?php echo $dtstart->format('l, j F Y, ga'); ?>
+							</time></p>
+							
+							<p class="location">Davison Room, <a href="http://www.map.harvard.edu/?ctrx=759617&ctry=2962591&level=10&layers=Campus%20Base%20and%20Buildings,Bike%20Facilities,Map%20Text" target="_blank" class="icon-link-ext">Harvard University Music Building</a></p>
 
-			/* Start the Loop */
-			while ( have_posts() ) {
-				the_post();
-
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to overload this in a child theme then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-				echo 'Colloquium';
-
-			}
-			
-			echo '</div>';
-		}
-
+						</section>
+						<?php the_content(); ?>
+				</article>
+			<?php endwhile; ?>
+		<?php else: ?>
+		<?php endif;
 
 get_footer();
 

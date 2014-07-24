f4fa344001d226c1ac2dbb49579787e50720bfa9
delucis <swithinbank@gmail.com>
2014-07-24 18:53:55 +0200

    Added custom post types to ‘At a Glance’ dashboard
    Credit:
    http://www.trickspanda.com/2014/03/add-custom-post-types-glance-dashboar
    d-widget-wordpress/

--- functions.php
+++ functions.php
@@ -322,6 +322,31 @@ function my_dtstart_orderby( $query ) {
 	}
 }
 
+// Add custom post types to dashboard ‘At a Glance’ module
+add_action( 'dashboard_glance_items', 'cpad_at_glance_content_table_end' );
+function cpad_at_glance_content_table_end() {
+    $args = array(
+        'public' => true,
+        '_builtin' => false
+    );
+    $output = 'object';
+    $operator = 'and';
+
+    $post_types = get_post_types( $args, $output, $operator );
+    foreach ( $post_types as $post_type ) {
+        $num_posts = wp_count_posts( $post_type->name );
+        $num = number_format_i18n( $num_posts->publish );
+        $text = _n( $post_type->labels->singular_name, $post_type->labels->name, intval( $num_posts->publish ) );
+        if ( current_user_can( 'edit_posts' ) ) {
+            $output = '<a href="edit.php?post_type=' . $post_type->name . '">' . $num . ' ' . $text . '</a>';
+            echo '<li class="post-count ' . $post_type->name . '-count">' . $output . '</li>';
+            } else {
+            $output = '<span>' . $num . ' ' . $text . '</span>';
+                echo '<li class="post-count ' . $post_type->name . '-count">' . $output . '</li>';
+            }
+    }
+}
+
 // Enable Featured Image for Member Custom Post Type
 if ( function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails', array( 'member', 'concert', 'miscevent' ) );

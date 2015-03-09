75eee63d94e0013ff3818c5ede0e575d3fed9e27
delucis <swithinbank@gmail.com>
2015-03-08 20:50:39 -0400

    Added basic 404 body copy, Pusheen gif, and homepage link

--- 404.php
+++ 404.php
@@ -1,20 +1,20 @@
 <?php
-
 get_header();
+?>
 
-		if ( have_posts() ) : ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class('p-section primary entry'); ?>>
-					<h2 class="post-title fname"><?php the_title(); ?></h2>
-					<?php if (current_user_can('edit_post')) : ?>
-						<a href="<?php echo get_edit_post_link(); ?>" class="edit-button">Edit</a>
-					<?php endif; ?>
-						<?php the_content(); ?>
-				</article>
-			<?php endwhile; ?>
-		<?php else: ?>
-		<?php endif;
+<article id="404" class="404-error member type-member status-publish has-post-thumbnail p-section no-primary">
+	<h2 class="post-title fname entry-title">404</h2>
+	<section class="secondary clearfix">
+		<h3>Oops! Sorry, that page doesn’t exist…</h3>
+		<div class="featured-img">
+			<img src="<?php echo get_bloginfo( 'template_directory' ) ?>/img/404.gif" alt="Sad cat">
+		</div>
+	</section>
+	<section class="composers-link">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Back to the home page »</a>
+	</section>
+</article>
 
+<?php
 get_footer();
-
 ?>

644da66cb9f5ebb6c243fb92b3af29d0f94cc43a
delucis <swithinbank@gmail.com>
2017-09-04 18:39:57 +0200

    Remove room indication from location on colloquium pages.

--- single-colloquium.php
+++ single-colloquium.php
@@ -20,8 +20,8 @@ get_header();
 							<p class="dtstart"><time class="value" datetime="<?php echo $dtstart->format('Y-m-d\TH:i:sO'); ?>">
 								<?php echo $dtstart->format('l, j F Y, ga'); ?>
 							</time></p>
-							
-							<p class="location map-popup">Davison Room, <a href="https://www.google.com/maps/place/Music+Bldg,+Harvard+University,+Cambridge,+MA+02138/@42.3769058,-71.1170215,15z/data=!4m2!3m1!1s0x89e3774164253f4d:0x4139366065ac28ee" class="icon-location">Harvard University Music Building</a></p>
+
+							<p class="location map-popup"><a href="https://www.google.com/maps/place/Music+Bldg,+Harvard+University,+Cambridge,+MA+02138/@42.3769058,-71.1170215,15z/data=!4m2!3m1!1s0x89e3774164253f4d:0x4139366065ac28ee" class="icon-location">Harvard University Music Building</a></p>
 
 						</section>
 						<?php if (get_field('photo')) {

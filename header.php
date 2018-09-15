37e036184130c1f995cc438e0c4db59e609715d7
delucis <swithinbank@gmail.com>
2018-09-15 01:11:28 -0400

    Remove Browse Happy old IE warnings

--- header.php
+++ header.php
@@ -5,10 +5,7 @@
 ?>
 
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html class="no-js">
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -97,9 +94,6 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?> lang="en">
-		<!--[if lt IE 7]>
-			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-		<![endif]-->
 		<div id="page" class="site">
 
 			<header id="masthead" class="site-header js-header clearfix" role="banner">

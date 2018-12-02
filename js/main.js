6f8a2be42f9c0b0e08ef8316effc14d2c5467958
delucis <swithinbank@gmail.com>
2018-12-01 22:26:39 -0500

    style(js): Apply StandardJS code style to main.js

--- js/main.js
+++ js/main.js
@@ -1,60 +1,56 @@
 // Mobile menu functionality (pattern from http://a11yproject.com/)
-(function(document, window, undefined) {
-
-  'use strict';
+(function (document, window) {
+  'use strict'
 
   // Vars
-  var header = document.querySelector('.js-header'),
-    menu = document.querySelector('.js-menu'),
-    menuButton = document.createElement('button');
+  var header = document.querySelector('.js-header')
+  var menu = document.querySelector('.js-menu')
+  var menuButton = document.createElement('button')
 
   // Button properties
-  menuButton.classList.add('menu-button');
-  menuButton.setAttribute('id', 'menu-button');
-  menuButton.setAttribute('aria-label', 'Menu');
-  menuButton.setAttribute('aria-expanded', 'false');
-  menuButton.setAttribute('aria-controls', 'menu');
-  menuButton.innerHTML = '<span aria-hidden="true">Menu</span>';
+  menuButton.classList.add('menu-button')
+  menuButton.setAttribute('id', 'menu-button')
+  menuButton.setAttribute('aria-label', 'Menu')
+  menuButton.setAttribute('aria-expanded', 'false')
+  menuButton.setAttribute('aria-controls', 'menu')
+  menuButton.innerHTML = '<span aria-hidden="true">Menu</span>'
 
   // Menu properties
-  menu.setAttribute('aria-hidden', 'true');
-  menu.setAttribute('aria-labelledby', 'menu-button');
+  menu.setAttribute('aria-hidden', 'true')
+  menu.setAttribute('aria-labelledby', 'menu-button')
 
   // Add button to page
-  header.insertBefore(menuButton, menu);
+  header.insertBefore(menuButton, menu)
 
   // Handle button click event
   menuButton.addEventListener('click', function () {
-
     // If active...
     if (menu.classList.contains('active')) {
       // Hide
-      header.classList.remove('active');
-      menu.classList.remove('active');
-      menu.setAttribute('aria-hidden', 'true');
-      menuButton.setAttribute('aria-expanded', 'false');
-      menuButton.classList.remove('active');
+      header.classList.remove('active')
+      menu.classList.remove('active')
+      menu.setAttribute('aria-hidden', 'true')
+      menuButton.setAttribute('aria-expanded', 'false')
+      menuButton.classList.remove('active')
     } else {
       // Show
-      header.classList.add('active');
-      menu.classList.add('active');
-      menu.setAttribute('aria-hidden', 'false');
-      menuButton.setAttribute('aria-expanded', 'true');
-      menuButton.classList.add('active');
+      header.classList.add('active')
+      menu.classList.add('active')
+      menu.setAttribute('aria-hidden', 'false')
+      menuButton.setAttribute('aria-expanded', 'true')
+      menuButton.classList.add('active')
 
       // Set focus on first link
-      menu.children[0].children[0].children[0].focus();
+      menu.children[0].children[0].children[0].focus()
     }
-  }, false);
-
-})(document, window);
-
+  }, false)
+})(document, window)
 
 // Replace jQuery’s $(document).ready
-function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-    fn();
+function ready (fn) {
+  if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+    fn()
   } else {
-    document.addEventListener('DOMContentLoaded', fn);
+    document.addEventListener('DOMContentLoaded', fn)
   }
 }

f4e15532568e10da98605bf8ce711f1d9475ff97
delucis <swithinbank@gmail.com>
2019-02-24 18:32:31 -0500

    fix(components): Display non-oEmbed `iframe`s with a hack

--- components/responsive_embed.php
+++ components/responsive_embed.php
@@ -16,6 +16,15 @@ if (!function_exists('responsive_embed')) {
             // add data-src and lozad class for lazy loading
             $attrs = 'data-src="' . $src . '" class="lozad"';
             $media = str_replace('></iframe>', ' ' . $attrs . '></iframe>', $media);
+        } elseif (strncmp($media, '<a', 2) === 0) {
+            // if an iframe fails to generate $media is passed as a link tag
+            // in that case, try to show a basic iframe with the link’s source
+            // as the iframe’s source
+            preg_match('/href="(.+?)"/', $media, $href_matches);
+            if (isset($href_matches[1])) {
+                $src = $href_matches[1];
+                $media = '<iframe class="lozad" scrolling="no" frameborder="no" data-src="' . $src . '"></iframe>';
+            }
         }
         $html = '<span class="embed-container">';
         $html .= $media;

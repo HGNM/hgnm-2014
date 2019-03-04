46fd64985baf3d2a9980067708d993b8662f529a
delucis <swithinbank@gmail.com>
2019-03-03 19:15:09 -0500

    feat(components): Create `responsive_card_list` component
    Format an array of items as a grid of cards that collapse to a 
    horizontally scrolling widget on mobile

---
+++ components/responsive_card_list.php
@@ -0,0 +1,22 @@
+<?php
+if (!function_exists('responsive_card_list')) {
+    /**
+     * Format an array of items as a grid of cards that collapse to a
+     * horizontally scrolling widget on mobile
+     * @param  array $opts          Options array
+     * @param  array $opts['cards'] Array of HTML strings to wrap
+     * @return string HTML string for the responsive card list component
+     */
+    function responsive_card_list($opts)
+    {
+        $cards = isset($opts['cards']) ? $opts['cards'] : array();
+        if (!is_array($cards)) return '';
+        $html = '<div class="responsive-cards"><ul class="responsive-cards__list">';
+        foreach ($cards as $card) {
+            $html .= '<li class="responsive-cards__item">' . $card . '</li>';
+        }
+        $html .= '</ul></div>';
+        return $html;
+    }
+}
+?>
